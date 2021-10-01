aaef8e010f96bbbf3fa20dec0972ea9f1cdd34f8
Pedro Sanchez <pedrolampo50@gmail.com>
2021-10-01 16:27:09 -0300

    clase 9 - eventos

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
-// Clase Persona con un constructor de objetos
-class Persona {
-  constructor(nombre, edad, sexo) {
-    this.nombre = nombre;
-    this.edad = edad;
-    this.sexo = sexo;
-  }
+let array = [
+    'Buenos días!',
+    'Buenas tardes!',
+    'Buenas noches!',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae optio cumque doloremque veniam quos eaque architecto odio praesentium, voluptatem esse mollitia quidem deleniti pariatur doloribus quae omnis et tenetur id!',
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae optio cumque doloremque veniam quos eaque architecto odio praesentium, voluptatem esse mollitia quidem deleniti pariatur doloribus quae omnis et tenetur id!',
+];
+
+function crearTexto() {
+    let btn = document.getElementsByClassName('btn');
+    let textoAqui = document.getElementById('textoAqui');
+
+    for (let i = 0; i < btn.length; i++) {
+        let button = btn[i];
+        button.addEventListener('click', function () {
+            let texto = document.createElement('div');
+            texto.innerText = `${array[i]}`;
+            textoAqui.appendChild(texto);
+        });
+    }
 }
 
-// Array que va a contener a los objetos
-const personas = [];
-
-// Objetos creados y pusheas al array "personas"
-personas.push(new Persona('Julio', 39, 'Masculino'));
-personas.push(new Persona('María', 25, 'Femenino'));
-personas.push(new Persona('Eve', 27, 'Femenino'));
-personas.push(new Persona('Darío', 33, 'Masculino'));
-
-// Función que compara los parametros edad
-const ordenar = (a, b) => {
-  if (a.edad < b.edad) {
-    return -1;
-  } else if (a.edad > b.edad) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
-
-// El método sort() toma como parámetro la función "ordenar", ordenando a los objetos según el valor que la misma devuelve
-console.log(personas.sort(ordenar));
+crearTexto();
